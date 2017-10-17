63aed6e275d7c350b1e92e11ff00b10581c95e19
breadthe <constantin.cojocaru@breaktech.com>
2017-10-17 10:59:04 -0500

    Import Nunito Google font and replace primary Bulma font with it

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -71,7 +71,7 @@
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  /*@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
   * {
     box-sizing: border-box;
@@ -151,5 +151,5 @@
   .doc button.alt {
     color: #42b983;
     background-color: transparent;
-  }
+  }*/
 </style>
