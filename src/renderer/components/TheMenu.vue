2d16a1c6fe4eea2885213c484220115ada4df903
breadthe <constantin.cojocaru@breaktech.com>
2017-10-17 15:28:45 -0500

    Make menu dynamic and selected menu options dynamic based on route

--- src/renderer/components/TheMenu.vue
+++ src/renderer/components/TheMenu.vue
@@ -1,15 +1,44 @@
 <template>
     <aside class="menu">
-        <ul class="menu-list">
-            <li><a href="/#/"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Home</a></li>
-            <li><a href="/#/settings"><i class="fa fa-gear" aria-hidden="true"></i>&nbsp;Settings</a></li>
-            <li><a href="/#/about"><i class="fa fa-question" aria-hidden="true"></i>&nbsp;About</a></li>
+        <ul class="menu-list" v-if="menuItems.length">
+            <li v-for="item in menuItems">
+                <a :href="item.path" @click="setMenuItemActive" ref="menuItem">
+                    <i :class="['fa', 'fa-' + item.logo]" aria-hidden="true"></i>&nbsp;{{ item.name }}
+                </a>
+            </li>
         </ul>
     </aside>
 </template>
 
 <script>
   export default {
-    name: 'the-menu'
+    name: 'the-menu',
+    data: function () {
+      return {
+        menuItems: [
+          { path: '/#/', name: 'Home', logo: 'home', active: false },
+          { path: '/#/settings', name: 'Settings', logo: 'gear', active: false },
+          { path: '/#/about', name: 'About', logo: 'question', active: false }
+        ],
+        menuItemIsSelected: true
+      }
+    },
+    methods: {
+      setMenuItemActive: function (e) {
+        // Loop through each menu item and remove the 'is-active' class
+        this.$refs.menuItem.forEach(i => { i.classList.remove('is-active') })
+
+        // Set 'is-active' on the current item
+        e.target.setAttribute('class', 'is-active')
+      }
+    },
+    computed: {
+    },
+    mounted: function () {
+      // Check if the current route is Home, in which case make the Home menu entry active
+      if (this.$route.path === '/') {
+        this.$refs.menuItem[0].setAttribute('class', 'is-active')
+      }
+    }
   }
 </script>
