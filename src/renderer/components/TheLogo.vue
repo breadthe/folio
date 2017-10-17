9bb7f562230f8e19a435449b8356669bdee7f3c0
breadthe <constantin.cojocaru@breaktech.com>
2017-10-17 16:28:14 -0500

    Tag v0.0.1 alpha

--- src/renderer/components/TheLogo.vue
+++ src/renderer/components/TheLogo.vue
@@ -2,7 +2,7 @@
     <div class="app-logo has-text-centered">
         <i class="fa fa-book fa-4x" aria-hidden="true"></i>
         <div class="tags has-addons">
-            <span class="tag is-light">beta</span>
+            <span class="tag is-light">alpha</span>
             <span class="tag is-white">0.0.1</span>
         </div>
     </div>
