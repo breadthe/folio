618c07b320b2dede67c13900c1253942f652ad86
breadthe <constantin.cojocaru@breaktech.com>
2018-01-22 16:23:05 -0600

    Update About page

--- src/renderer/components/AboutPage.vue
+++ src/renderer/components/AboutPage.vue
@@ -4,12 +4,13 @@
 
       <p>This app is build with love and:
         <ul>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://vuejs.org')">Vue.js</a></li>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://electronjs.org')">Electron</a></li>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://bulma.io')">Bulma</a></li>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://tailwindcss.com')">TailwindCSS</a></li>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://socket.io')">Socket.io</a></li>
-          <li><a class="storyblok-button blue tw-float-left tw-m-1" @click="open('https://greensock.com')">Green Sock Animation Platform (GSAP)</a></li>
+          <li><a @click="open('https://vuejs.org/')">Vue.js</a></li>
+          <li><a @click="open('https://electronjs.org/')">Electron</a></li>
+          <li><a @click="open('https://bulma.io/')">Bulma</a></li>
+          <li><a @click="open('https://tailwindcss.com/')">TailwindCSS</a></li>
+          <li><a @click="open('https://socket.io/')">Socket.io</a></li>
+          <li><a @click="open('https://greensock.com/')">Green Sock Animation Platform (GSAP)</a></li>
+          <li><a @click="open('https://masonry.desandro.com/')">Masonry Layout</a></li>
         </ul>
       </p>
 
