e1ea1c18fcdfb36fa23dd27b081c4c03bac50ab8
breadthe <constantin.cojocaru@breaktech.com>
2018-01-23 17:06:58 -0600

    Stats on Dashboard

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="section">
-    <section class="container tw-h-screen">
+    <section class="container tw-h-full">
 
-      <h6>Dashboard</h6>
+      <table class="table is-fullwidth">
+          <thead>
+            <tr>
+                <th>coin</th>
+                <th>qty</th>
+                <th>total</th>
+            </tr>
+            <tr>
+                <th colspan="3" class="tw-bg-black">
+                  <p class="tw-text-right"><span class="tw-text-xl">${{ formatCurrency(portfolioSummary.totalUSDValue) }}</span></p>
+                </th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr v-for="coin in portfolio" :key="coin.market">
+                  <td><div class="coin-sprite tw-mt-1 tw-mr-1" :class="coin.symbol"></div>&nbsp;{{ coin.name }}</td>
+                  <td>{{ coin.qty }}</td>
+                  <td class="tw-text-right">${{ formatCurrency(USDValue(coin.qty, coin.lastTrade.details.price)) }}</td>
+              </tr>
+          </tbody>
+      </table>
 
-      <div>Total USD value: ${{ portfolioSummary.totalUSDValue }}</div>
 
     </section>
   </div>
@@ -21,21 +40,29 @@
         pageSubTitle: 'Home subtitle'
       }
     },
+    methods: {
+      formatCurrency: function (amount) {
+        return amount.toFixed(2).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
+      },
+      USDValue: function (qty, price) {
+        return parseFloat(qty) * parseFloat(price)
+      }
+    },
     computed: {
+      portfolio: function () {
+        return store.getters.coins.filter(coin => parseFloat(coin.qty) > 0)
+      },
       portfolioSummary: function () {
+        const self = this
         // portfolio = coins for which I have an amount > 0
-        const portfolio = store.getters.coins.filter(coin => parseFloat(coin.qty) > 0)
+        const portfolio = this.portfolio
         let totalBTCValue = 0
         let totalUSDValue = 0
 
         portfolio.forEach(function (coin) {
-          totalUSDValue += parseFloat(coin.qty) * parseFloat(coin.lastTrade.details.price)
+          totalUSDValue += self.USDValue(coin.qty, coin.lastTrade.details.price)
         })
-        console.log({
-          totalCoinsWatched: portfolio.length,
-          totalBTCValue: totalBTCValue,
-          totalUSDValue: totalUSDValue
-        })
+
         return {
           totalCoinsWatched: portfolio.length,
           totalBTCValue: totalBTCValue,
