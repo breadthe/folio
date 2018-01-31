f0fa98cae698a373d845d2ec60109257d8a5c52c
breadthe <constantin.cojocaru@breaktech.com>
2018-01-31 17:06:40 -0600

    Improve styling of OrderBy component

--- src/renderer/components/OrderBy.vue
+++ src/renderer/components/OrderBy.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="sort-wrapper tw-mb-2">
       <div>
-        <a :class="{'active': sortBy === 'name-asc'}" @click="orderBy('name-asc')"><span class="tw-text-sm">Name &uarr;</span></a>
-        <a :class="{'active': sortBy === 'name-desc'}" @click="orderBy('name-desc')"><span class="tw-text-sm">Name &darr;</span></a>
-        <a :class="{'active': sortBy === 'qty-asc'}" @click="orderBy('qty-asc')"><span class="tw-text-sm">Qty &uarr;</span></a>
-        <a :class="{'active': sortBy === 'qty-desc'}" @click="orderBy('qty-desc')"><span class="tw-text-sm">Qty &darr;</span></a>
+        <button :disabled="sortBy === 'name-asc'" @click="orderBy('name-asc')"><span class="tw-text-sm">Name &uarr;</span></button>
+        <button :disabled="sortBy === 'name-desc'" @click="orderBy('name-desc')"><span class="tw-text-sm">Name &darr;</span></button>
+        <button :disabled="sortBy === 'qty-asc'" @click="orderBy('qty-asc')"><span class="tw-text-sm">Qty &uarr;</span></button>
+        <button :disabled="sortBy === 'qty-desc'" @click="orderBy('qty-desc')"><span class="tw-text-sm">Qty &darr;</span></button>
       </div>
     </div>
 </template>
